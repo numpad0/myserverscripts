f3b2cc638ed6108c6a854f47fed4403283c92dd3
numpad0 <triphenylmethane@gmail.com>
2016-04-16 23:17:51 +0900

    Added files via upload

--- createnewuser.sh
+++ createnewuser.sh
@@ -3,11 +3,13 @@
 if [ ! -z $1 ]; then
 	username=$1
 	useradd -m -d /home/$1 -G sudo -s /bin/bash $1
+	sudo passwd -e $1
+	
 	mkdir -p /home/$1/.ssh
 	wget http://github.com/$1.keys -O /home/$1/.ssh/authorized_keys
 	
 	chown -R $1 /home/$1/.ssh
-	chmod 701 $1 /home/$1/.ssh
+	chmod 701 /home/$1/.ssh
 
 	chown -R $1 /home/$1/.ssh/authorized_keys
 	chmod 600 /home/$1/.ssh/authorized_keys
